8711b86e83839346b601c69f9f7ef5fe4edd3d72
Areg Shahbazian <areg.shahbazian@ritense.com>
2016-07-13 11:14:57 +0200

    Added AngularJS minimum

--- src/main/java/com/myspingbootapp/controllers/AngularJSController.java
+++ src/main/java/com/myspingbootapp/controllers/AngularJSController.java
@@ -3,15 +3,19 @@ package com.myspingbootapp.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-public class GreetingController {
+@RequestMapping(value = "ng")
+public class AngularJSController
+{
 
-    @RequestMapping("/greeting")
-    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
-        model.addAttribute("name", name);
-        return "greeting";
-    }
+	@RequestMapping(value = "hello", method = RequestMethod.GET)
+	public String hello()
+	{
+		return "ng/hello";
+	}
+
 
 }
---
+++ src/main/java/com/myspingbootapp/controllers/SpringMVCController.java
@@ -0,0 +1,22 @@
+package com.myspingbootapp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping(value = "mvc")
+public class SpringMVCController
+{
+
+    @RequestMapping(value = "hello", method = RequestMethod.GET)
+    public String hello(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "mvc/hello";
+    }
+    
+    
+
+}
