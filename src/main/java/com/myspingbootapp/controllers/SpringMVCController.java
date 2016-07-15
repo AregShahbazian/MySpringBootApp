3db8b492f9085363f55997c8670507d92a41c50b
Areg Shahbazian <areg.shahbazian@ritense.com>
2016-07-15 16:54:24 +0200

    Thymeleaf iterations

--- src/main/java/com/myspingbootapp/controllers/SpringMVCController.java
+++ src/main/java/com/myspingbootapp/controllers/SpringMVCController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.ArrayList;
+
 /**
  * Test on
  * http://localhost:8080/mvc/hello?name=Foo
@@ -19,7 +21,12 @@ public class SpringMVCController
 	@RequestMapping(value = "hello", method = RequestMethod.GET)
 	public String hello(@RequestParam(value = "name", required = false, defaultValue = "World") String name, Model model)
 	{
+		ArrayList<String> arrayList = new ArrayList<>();
+		arrayList.add("bar");
+		arrayList.add("foo");
+
 		model.addAttribute("name", name);
+		model.addAttribute("list", arrayList);
 		return "hello";
 	}
 
