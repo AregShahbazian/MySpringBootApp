9669292400dea66c223dce24b56940255b220921
areguva <areg.uvagm@gmail.com>
2016-07-19 21:56:43 +0200

    get controller

--- src/main/java/com/myspingbootapp/controllers/UserController.java
+++ src/main/java/com/myspingbootapp/controllers/UserController.java
@@ -3,7 +3,7 @@ package com.myspingbootapp.controllers;
 import org.springframework.web.bind.annotation.*;
 
 import com.myspingbootapp.domain.model.User;
-import com.myspingbootapp.persistence.repositories.specific.UserRepository;
+import com.myspingbootapp.persistence.repositories.GenericRepository;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -12,24 +12,38 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 
 @RestController
-@RequestMapping(value = "DomainEntitySimpleEndpoint")
+@RequestMapping(value = "User")
 @Api(produces = "application/json")
+@Transactional
 public class UserController {
 
 	@Autowired
-	Environment env;
+	Environment environment;
 
 	@Autowired
-	UserRepository userRepository;
+	GenericRepository genericRepository;
 
 	@RequestMapping(value = "Add", method = RequestMethod.POST, produces = { "application/json" })
 	@ApiOperation(value = "Add a User to the database", notes = "Not secured", response = ResponseEntity.class)
-	public ResponseEntity addUser(@RequestBody User user) {
+	public ResponseEntity addUser(@RequestBody User userInput) {
 
-		Long id = userRepository.save(user);
-		return new ResponseEntity(userRepository.getUserById(id), HttpStatus.OK);
+		Long id = genericRepository.save(userInput);
+		
+		User user = genericRepository.getObjectById(id, User.class);
+
+		return new ResponseEntity(user, HttpStatus.OK);
+	}
+
+	@RequestMapping(value = "Get/{id}", method = RequestMethod.GET, produces = { "application/json" })
+	@ApiOperation(value = "Get a User from the database, using the id", notes = "Not secured", response = ResponseEntity.class)
+	public ResponseEntity getUser(@PathVariable("id") Long id) {
+
+		User user = genericRepository.getObjectById(id, User.class);
+
+		return new ResponseEntity(user, HttpStatus.OK);
 
 	}
 
--- src/main/java/com/myspingbootapp/domain/model/User.java
+++ src/main/java/com/myspingbootapp/domain/model/User.java
@@ -5,6 +5,7 @@ import java.util.Set;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -33,13 +34,13 @@ public class User {
 	private String username;
 
 	@ApiModelProperty(value = "Articles created by the user")
-	@OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
+	@OneToMany(cascade = CascadeType.ALL, mappedBy = "owner", fetch = FetchType.EAGER)
 	private Set<Article> articles;// TOOD: try: initialize with new
 
 	@ApiModelProperty(value = "Orders placed by the user")
-	@OneToMany(cascade = CascadeType.ALL, mappedBy = "user")
+	@OneToMany(cascade = CascadeType.ALL, mappedBy = "user", fetch = FetchType.EAGER)
 	private Set<ArticleOrder> orders;
-	
+
 	public User() {
 		super();
 	}
