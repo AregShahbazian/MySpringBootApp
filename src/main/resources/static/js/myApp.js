9e0fbeb229f75965ba3ef4a22880215c1fbc69f4
Areg Shahbazian <areg.shahbazian@ritense.com>
2016-07-26 17:29:05 +0200

    Angular model

--- src/main/resources/static/js/myApp.js
+++ src/main/resources/static/js/myApp.js
@@ -2,4 +2,12 @@ var app = angular.module('myApp', []);
 app.controller('myCtrl', function ($scope) {
 	$scope.firstName = "John";
 	$scope.lastName = "DoeCtrl";
+	$scope.names = ['Jani', 'Hege', 'Kai'];
+});
+
+app.directive("w3TestDirective", function () {
+	return {
+		restrict: "EACM",
+		template: "<h3>Made by a directive!</h3>"
+	};
 });
